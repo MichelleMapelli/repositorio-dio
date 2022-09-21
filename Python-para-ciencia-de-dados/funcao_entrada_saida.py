0bb7e5b75a0474fc05cc2eb70e3628f66cfbe604
MichelleMapelli <mi.mapelli@gmail.com>
2022-09-21 17:46:59 -0300

    Adicionando arquivo de entrada e saida Python

---
+++ Python-para-ciencia-de-dados/funcao_entrada_saida.py
@@ -0,0 +1,6 @@
+nome = input("Informe o seu nome: ")
+sobrenome = input("Qual o seu sobrenome: ")
+print(f'Meu nome é {nome} {sobrenome}')
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n")
+print(nome, sobrenome, sep="#")
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ### Repositório DIO
 
 O Objetivo desse desafio é dar ao aluno autonomia de criar sozinho um repositório e posteriormente utilizá-lo 
+
+## Os exercícios Python estarão nesse repositório
