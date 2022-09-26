865ad933b8ff2205252337272e640ddd1a7372db
MichelleMapelli <mi.mapelli@gmail.com>
2022-09-26 10:50:37 -0300

    Adicionando mais uma parte do conteúdo de string

---
+++ Python-para-ciencia-de-dados/manipulacao de string/fatiamento_string.py
@@ -0,0 +1,12 @@
+# Start, Stop e Step
+nome = "Guilherme Arthur de Carvalho"
+
+print(nome[0])
+print(nome[-1])
+print(nome[:9]) # termina no nono
+print(nome[10:]) # começa no décimo
+print(nome[10:16]) # começa no 10 e termina com 16
+print(nome[10:16:2]) # de dois em dois
+print(nome[:]) 
+print(nome[::-1]) # espelha string (de trás pra frente)
+
---
+++ Python-para-ciencia-de-dados/manipulacao de string/string_multiplas_linhas.py
@@ -0,0 +1,18 @@
+nome = "Michelle"
+
+mensagem = f"""Olá meu nome  é {nome}, 
+    Eu estou aprendendo Python"""
+
+print(mensagem)
+
+
+print(f"""
+
+    ############### MENU ##################
+
+    1 - Depositar
+    2 - Sacar
+    0 - Sacar
+
+        Obrigada por usar nosso sistema
+""")
