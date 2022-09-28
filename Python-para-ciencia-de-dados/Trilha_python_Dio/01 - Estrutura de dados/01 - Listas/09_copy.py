84ead04d08687c2fa58e24a148cb50cc287a84ee
MichelleMapelli <mi.mapelli@gmail.com>
2022-09-28 14:52:48 -0300

    testando algumas funcoes

--- Python-para-ciencia-de-dados/Trilha_python_Dio/01 - Estrutura de dados/01 - Listas/09_copy.py
+++ Python-para-ciencia-de-dados/Trilha_python_Dio/01 - Estrutura de dados/01 - Listas/09_copy.py
@@ -3,3 +3,12 @@ lista = [1, "Python", [40, 30, 20]]
 lista.copy()
 
 print(lista)  # [1, "Python", [40, 30, 20]]
+
+l2 = lista.copy()
+
+print(id(l2), id(lista))
+
+l2[0] = 2
+
+print(l2)
+print(lista)
--- Python-para-ciencia-de-dados/Trilha_python_Dio/01 - Estrutura de dados/01 - Listas/14_remove.py
+++ Python-para-ciencia-de-dados/Trilha_python_Dio/01 - Estrutura de dados/01 - Listas/14_remove.py
@@ -1,5 +1,5 @@
 linguagens = ["python", "js", "c", "java", "csharp"]
 
-linguagens.remove("c")
+linguagens.remove("js")
 
 print(linguagens)  # ["python", "js", "java", "csharp"]
